027c433b1a8d81ecac930b38203e1e169d758f59
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-05-18 22:38:52 -0500

    MVP - 1 Stretch completed

--- auth/router.js
+++ auth/router.js
@@ -6,9 +6,16 @@ const { isValid } = require('../users/user-service.js');
 
 router.post('/register', (req, res) => {
 	const credentials = req.body;
-	if (isValid(req.body)) {
-		Users.findBy({ username: username })
+	if (isValid(credentials)) {
+		const rounds = process.env.BCRYPT_ROUNDS || 8;
+		// hash the password
+		const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+		credentials.password = hash;
+		//save the user to the database
+		Users.add(credentials)
 			.then((user) => {
+				req.session.loggedIn === true;
 				res.status(201).json({ data: user });
 			})
 			.catch((error) => {
@@ -23,17 +30,20 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-	const { username, password } = req.body;
+	const { username, password } = req.body; //separate username and password
 	if (isValid(req.body)) {
-		const rounds = process.env.BCRYPT_ROUNDS || 8;
-		// hash the password
-		const hash = bcryptjs.hashSync(credentials.password, rounds);
-
-		credentials.password = hash;
-		//save the user to the database
-		Users.add(credentials)
-			.then((user) => {
-				res.status(201).json({ data: user });
+		Users.findBy({ username: username }) //search for the user by username
+			.then(([user]) => {
+				// compare the password the hash stored in the database
+				if (user && bcryptjs.compareSync(password, user.password)) {
+					//compare the database hashed password and the user password
+					//we can save info about the client inside the session (req.session)
+					req.session.loggedIn = true;
+					req.session.user = user;
+					res.status(200).json({ message: 'You are logged in' });
+				} else {
+					res.status(401).json({ message: 'Invalid credentials' });
+				}
 			})
 			.catch((error) => {
 				res.status(500).json({ message: error.message });
@@ -43,6 +53,22 @@ router.post('/login', (req, res) => {
 			message:
 				'Please provide username and password & password should be string',
 		});
+	}
+});
+
+router.get('/logout', (req, res) => {
+	if (req.session) {
+		req.session.destroy((error) => {
+			if (err) {
+				res.status(500).json({
+					message: 'There was an error loggin you our, please try again',
+				});
+			} else {
+				res.status(204).end();
+			}
+		});
+	} else {
+		res.status(204).end();
 	}
 });
 
